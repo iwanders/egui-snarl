a391f9b8b97d9b6e6a6481ffb1ff855984ec08e7
Ivor Wanders <ivor@iwanders.net>
2024-03-21 19:30:00 -0400

    Add a more minimal example.

---
+++ examples/minimal.rs
@@ -0,0 +1,369 @@
+use eframe::{App, CreationContext};
+use egui::{Color32, Ui};
+use egui_snarl::{
+    ui::{PinInfo, SnarlStyle, SnarlViewer},
+    InPin, NodeId, OutPin, Snarl,
+};
+
+const STRING_COLOR: Color32 = Color32::from_rgb(0x00, 0xb0, 0x00);
+const NUMBER_COLOR: Color32 = Color32::from_rgb(0xb0, 0x00, 0x00);
+const UNTYPED_COLOR: Color32 = Color32::from_rgb(0xb0, 0xb0, 0xb0);
+
+#[derive(Clone, serde::Serialize, serde::Deserialize)]
+enum DemoNode {
+    /// Node with single input.
+    /// Displays the value of the input.
+    Sink,
+
+    /// Value node with a single output.
+    /// The value is editable in UI.
+    Number(f64),
+
+    /// Value node with a single output.
+    String(String),
+}
+
+impl DemoNode {
+    
+}
+
+struct DemoViewer;
+
+impl SnarlViewer<DemoNode> for DemoViewer {
+    #[inline]
+    fn connect(&mut self, from: &OutPin, to: &InPin, snarl: &mut Snarl<DemoNode>) {
+        // Validate connection
+        match (&snarl[from.id.node], &snarl[to.id.node]) {
+            (DemoNode::Sink, _) => {
+                unreachable!("Sink node has no outputs")
+            }
+            (_, DemoNode::Sink) => {}
+            (_, DemoNode::Number(_)) => {
+                unreachable!("Number node has no inputs")
+            }
+            (_, DemoNode::String(_)) => {
+                unreachable!("String node has no inputs")
+            }
+        }
+
+        for &remote in &to.remotes {
+            snarl.disconnect(remote, to.id);
+        }
+
+        snarl.connect(from.id, to.id);
+    }
+
+    fn title(&mut self, node: &DemoNode) -> String {
+        match node {
+            DemoNode::Sink => "Sink".to_owned(),
+            DemoNode::Number(_) => "Number".to_owned(),
+            DemoNode::String(_) => "String".to_owned(),
+        }
+    }
+
+    fn inputs(&mut self, node: &DemoNode) -> usize {
+        match node {
+            DemoNode::Sink => 1,
+            DemoNode::Number(_) => 0,
+            DemoNode::String(_) => 0,
+        }
+    }
+
+    fn outputs(&mut self, node: &DemoNode) -> usize {
+        match node {
+            DemoNode::Sink => 0,
+            DemoNode::Number(_) => 1,
+            DemoNode::String(_) => 1,
+        }
+    }
+
+    fn show_input(
+        &mut self,
+        pin: &InPin,
+        ui: &mut Ui,
+        _scale: f32,
+        snarl: &mut Snarl<DemoNode>,
+    ) -> PinInfo {
+        match snarl[pin.id.node] {
+            DemoNode::Sink => {
+                assert_eq!(pin.id.input, 0, "Sink node has only one input");
+
+                match &*pin.remotes {
+                    [] => {
+                        ui.label("None");
+                        PinInfo::circle().with_fill(UNTYPED_COLOR)
+                    }
+                    [remote] => match snarl[remote.node] {
+                        DemoNode::Sink => unreachable!("Sink node has no outputs"),
+                        DemoNode::Number(value) => {
+                            assert_eq!(remote.output, 0, "Number node has only one output");
+                            ui.label(format_float(value));
+                            PinInfo::square().with_fill(NUMBER_COLOR)
+                        }
+                        DemoNode::String(ref value) => {
+                            assert_eq!(remote.output, 0, "String node has only one output");
+                            ui.label(format!("{:?}", value));
+                            PinInfo::triangle().with_fill(STRING_COLOR)
+                        }
+                    },
+                    _ => unreachable!("Sink input has only one wire"),
+                }
+            }
+            DemoNode::Number(_) => {
+                unreachable!("Number node has no inputs")
+            }
+            DemoNode::String(_) => {
+                unreachable!("String node has no inputs")
+            }
+        }
+    }
+
+    fn show_output(
+        &mut self,
+        pin: &OutPin,
+        ui: &mut Ui,
+        _scale: f32,
+        snarl: &mut Snarl<DemoNode>,
+    ) -> PinInfo {
+        match snarl[pin.id.node] {
+            DemoNode::Sink => {
+                unreachable!("Sink node has no outputs")
+            }
+            DemoNode::Number(ref mut value) => {
+                assert_eq!(pin.id.output, 0, "Number node has only one output");
+                ui.add(egui::DragValue::new(value));
+                PinInfo::square().with_fill(NUMBER_COLOR)
+            }
+            DemoNode::String(ref mut value) => {
+                assert_eq!(pin.id.output, 0, "String node has only one output");
+                let edit = egui::TextEdit::singleline(value)
+                    .clip_text(false)
+                    .desired_width(0.0)
+                    .margin(ui.spacing().item_spacing);
+                ui.add(edit);
+                PinInfo::triangle().with_fill(STRING_COLOR)
+            }
+        }
+    }
+
+    fn input_color(
+        &mut self,
+        pin: &InPin,
+        _style: &egui::Style,
+        snarl: &mut Snarl<DemoNode>,
+    ) -> Color32 {
+        match snarl[pin.id.node] {
+            DemoNode::Sink => {
+                assert_eq!(pin.id.input, 0, "Sink node has only one input");
+                match &*pin.remotes {
+                    [] => UNTYPED_COLOR,
+                    [remote] => match snarl[remote.node] {
+                        DemoNode::Sink => unreachable!("Sink node has no outputs"),
+                        DemoNode::Number(_) => NUMBER_COLOR,
+                        DemoNode::String(_) => STRING_COLOR,
+                    },
+                    _ => unreachable!("Sink input has only one wire"),
+                }
+            }
+            DemoNode::Number(_) => {
+                unreachable!("Number node has no inputs")
+            }
+            DemoNode::String(_) => {
+                unreachable!("String node has no inputs")
+            }
+        }
+    }
+
+    fn output_color(
+        &mut self,
+        pin: &OutPin,
+        _style: &egui::Style,
+        snarl: &mut Snarl<DemoNode>,
+    ) -> Color32 {
+        match snarl[pin.id.node] {
+            DemoNode::Sink => {
+                unreachable!("Sink node has no outputs")
+            }
+            DemoNode::Number(_) => NUMBER_COLOR,
+            DemoNode::String(_) => STRING_COLOR,
+        }
+    }
+
+    fn graph_menu(
+        &mut self,
+        pos: egui::Pos2,
+        ui: &mut Ui,
+        _scale: f32,
+        snarl: &mut Snarl<DemoNode>,
+    ) {
+        ui.label("Add node");
+        if ui.button("Number").clicked() {
+            snarl.insert_node(pos, DemoNode::Number(0.0));
+            ui.close_menu();
+        }
+        if ui.button("String").clicked() {
+            snarl.insert_node(pos, DemoNode::String("".to_owned()));
+            ui.close_menu();
+        }
+        if ui.button("Sink").clicked() {
+            snarl.insert_node(pos, DemoNode::Sink);
+            ui.close_menu();
+        }
+    }
+
+    fn node_menu(
+        &mut self,
+        node: NodeId,
+        _inputs: &[InPin],
+        _outputs: &[OutPin],
+        ui: &mut Ui,
+        _scale: f32,
+        snarl: &mut Snarl<DemoNode>,
+    ) {
+        ui.label("Node menu");
+        if ui.button("Remove").clicked() {
+            snarl.remove_node(node);
+            ui.close_menu();
+        }
+    }
+
+    fn has_on_hover_popup(&mut self, _: &DemoNode) -> bool {
+        true
+    }
+
+    fn show_on_hover_popup(
+        &mut self,
+        node: NodeId,
+        _inputs: &[InPin],
+        _outputs: &[OutPin],
+        ui: &mut Ui,
+        _scale: f32,
+        snarl: &mut Snarl<DemoNode>,
+    ) {
+        match snarl[node] {
+            DemoNode::Sink => {
+                ui.label("Displays anything connected to it");
+            }
+            DemoNode::Number(_) => {
+                ui.label("Outputs integer value");
+            }
+            DemoNode::String(_) => {
+                ui.label("Outputs string value");
+            }
+        }
+    }
+}
+
+pub struct DemoApp {
+    snarl: Snarl<DemoNode>,
+    style: SnarlStyle,
+}
+
+impl DemoApp {
+    pub fn new(cx: &CreationContext) -> Self {
+        let snarl = match cx.storage {
+            None => Snarl::new(),
+            Some(storage) => {
+                let snarl = storage
+                    .get_string("snarl")
+                    .and_then(|snarl| serde_json::from_str(&snarl).ok())
+                    .unwrap_or_else(Snarl::new);
+
+                snarl
+            }
+        };
+        // let snarl = Snarl::new();
+
+        let style = match cx.storage {
+            None => SnarlStyle::new(),
+            Some(storage) => {
+                let style = storage
+                    .get_string("style")
+                    .and_then(|style| serde_json::from_str(&style).ok())
+                    .unwrap_or_else(SnarlStyle::new);
+
+                style
+            }
+        };
+        // let style = SnarlStyle::new();
+
+        DemoApp { snarl, style }
+    }
+}
+
+impl App for DemoApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui_extras::install_image_loaders(ctx);
+
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            // The top panel is often a good place for a menu bar:
+
+            egui::menu::bar(ui, |ui| {
+                #[cfg(not(target_arch = "wasm32"))]
+                {
+                    ui.menu_button("File", |ui| {
+                        if ui.button("Quit").clicked() {
+                            ctx.send_viewport_cmd(egui::ViewportCommand::Close)
+                        }
+                    });
+                    ui.add_space(16.0);
+                }
+
+                egui::widgets::global_dark_light_mode_switch(ui);
+            });
+        });
+
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            self.snarl
+                .show(&mut DemoViewer, &self.style, egui::Id::new("snarl"), ui);
+        });
+    }
+
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        let snarl = serde_json::to_string(&self.snarl).unwrap();
+        storage.set_string("snarl", snarl);
+
+        let style = serde_json::to_string(&self.style).unwrap();
+        storage.set_string("style", style);
+    }
+}
+
+// When compiling natively:
+#[cfg(not(target_arch = "wasm32"))]
+fn main() -> eframe::Result<()> {
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([300.0, 220.0]),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "egui-snarl demo",
+        native_options,
+        Box::new(|cx| Box::new(DemoApp::new(cx))),
+    )
+}
+
+// When compiling to web using trunk:
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::WebRunner::new()
+            .start(
+                "egui_snarl_demo",
+                web_options,
+                Box::new(|cx| Box::new(DemoApp::new(cx))),
+            )
+            .await
+            .expect("failed to start eframe");
+    });
+}
+
+fn format_float(v: f64) -> String {
+    let v = (v * 1000.0).round() / 1000.0;
+    format!("{}", v)
+}
